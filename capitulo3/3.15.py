90bef5c49efa286cef1605371ea9d230ce1245bc
Eric Moritsuka <ericthmoritsuka@gmail.com>
2024-03-22 19:34:33 -0300

    added exercise to chapter 3

---
+++ capitulo3/3.15.py
@@ -0,0 +1,14 @@
+cigarettes = int(input("How many cigarettes a day do you smoke? "))
+
+print("\n")
+
+years = int(input("How many years have you been smoking? "))
+
+cigarettes = cigarettes * 365 * years
+
+minutes = cigarettes * 10
+days = minutes/60/24
+
+print("\n")
+
+print(f"You'll lose approximately {days:.0f} days because you have already smoked {cigarettes} cigarettes until now. Congrats...")
